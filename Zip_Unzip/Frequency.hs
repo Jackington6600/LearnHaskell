7b6d4613525914c7e66b20e7ec0a9ec87101edca
Jack Morrison <jack.morrison6600@gmail.com>
2016-10-19 16:16:36 +0100

    Added quicksort and finished frequency

--- Zip_Unzip/Frequency.hs
+++ Zip_Unzip/Frequency.hs
@@ -1,11 +1,16 @@
 -- Get the frequency of ASCII characters in a file.
--- TODO: Solve insert, currently destroys the accumulator
--- 'where' may not work
 
 frequency xs = frequency' xs []
 
-frequency' [] accum = accum
+-- frequency helper function to do the legwork.
+frequency' [] accum = quicksort accum
 frequency' (x:xs) accum = frequency' xs (insert [] x accum)
 
+-- insert the next given character into the list of pairs, incrementing if it is already
+-- present and appending to the end of the list ( initial frequency 1) if it is not already present.
 insert pre x [] = pre ++ [(x,1)]
 insert pre x ((a,n):as) = if x == a then pre ++ (a,n+1):as else insert (pre ++ [(a,n)]) x as
+
+-- Basic quicksort algorithm to put the list of pairs in descending order.
+quicksort [] = []
+quicksort ((x,y):xs) = quicksort [(a,b) | (a,b) <- xs, b > y] ++ [(x,y)] ++ quicksort [(a,b) | (a,b) <- xs, b <= y]
