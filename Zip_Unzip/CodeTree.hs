67de6d43cfacfc8548500c16eb52707e01cd924e
Jack Morrison <jack.morrison6600@gmail.com>
2016-10-21 11:58:39 +0100

    Added CodeTree and combined with Frequency

--- Zip_Unzip/CodeTree.hs
+++ Zip_Unzip/CodeTree.hs
@@ -1,8 +1,25 @@
-import Frequency
 
--- Tree that will encode and decode the text to reduce the file sizes
--- Uses frequency
+-- Get the frequency of ASCII characters in a file.
+frequency xs = frequency' xs []
 
-data Tree = Empty
-          | EndOr Tree
-          | Branch Tree Tree
+frequency&sort xs = quciksort (frequency' xs [])
+
+-- frequency helper function to do the legwork.
+frequency' [] accum = accum
+frequency' (x:xs) accum = frequency' xs (insert [] x accum)
+
+-- insert the next given character into the list of pairs, incrementing if it is already
+-- present and appending to the end of the list ( initial frequency 1) if it is not already present.
+insert pre x [] = pre ++ [(x,1)]
+insert pre x ((a,n):as) = if x == a then pre ++ (a,n+1):as else insert (pre ++ [(a,n)]) x as
+
+-- Basic quicksort algorithm to put the list of pairs in descending order.
+quicksort [] = []
+quicksort ((x,y):xs) = quicksort [(a,b) | (a,b) <- xs, b > y] ++ [(x,y)] ++ quicksort [(a,b) | (a,b) <- xs, b <= y]
+
+
+-- A tree that represents a collection of binary strings:
+data CodeTree = Empty            -- reject 
+              | End              -- accept and end
+              | Branch CodeTree CodeTree -- accept and proceed 
+              deriving Show
